96f123177cf465b61f4718660c623bc7209e7135
boywudicode <124773676+boywudicode@users.noreply.github.com>
2023-03-08 13:15:58 +0100

    make changes

--- some.js
+++ some.js
@@ -1,29 +1,26 @@
 let name = prompt ("what is your name");
-let message = alert("welcome");
+let message = "Welcome"
+let newMessage = message + " " + name;
+alert(newMessage);
 let age = prompt ("How old are you")
-let price = 50000
+const price = 50000
+let discount, newPrice
+function calDiscount(disc){
+     discount = (disc)*price;
+     newPrice = price - discount;
+    alert(`Thank You ${name},You have a discount of ${discount}`);}
 if (age<=13) {
-    let discount = (20/100)*price
-    let newPrice = price - discount
-    alert(`Thank You ${name},You have a discount of ${discount}`);
+    calDiscount(20/100)
 }
 else if (age<=18) {
-    discount = 40/100*price
-    newPrice = price - discount
-    alert(`Thank You ${name},You have a discount of ${discount}`);
+  calDiscount(40/100)
 }
 else if (age<=30) {
-    discount = 60/100*price
-    newPrice = price - discount
-    alert(`Thank You ${name},You have a discount of ${discount}`);
+    calDiscount(60/100)
 }
 else if (age<=65) {
-    discount = 80/100*price
-    newPrice = price - discount
-    alert(`Thank You ${name},You have a discount of ${discount}`);
+   caldiscount(80/100)
 }
-else (age>65) {
-    discount = 100/100*price
-    newPrice = price - discount
-    alert(`Thank You ${name},You have a discount of ${discount}`);
+else if(age>65) {
+   calDiscount(100/100)
 }
